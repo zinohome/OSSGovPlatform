96bee1ea4ddcecb17f211b54ac15b9cf2bad163b
zhangjun <ibmzhangjun@139.com>
2023-01-12 21:49:24 +0800

    Init

--- apps/admin/admin_implements/coladmin.py
+++ apps/admin/admin_implements/coladmin.py
@@ -10,12 +10,29 @@
 
 import traceback
 import simplejson as json
+from fastapi import Body, Depends
 
+from utils import datetime_util
 from utils.amis_admin import amis,admin
 from utils.amis_admin.amis import PageSchema
-
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    List,
+    Optional,
+    Pattern,
+    Tuple,
+    Type,
+    Union,
+)
+from starlette.requests import Request
 from models.db_models.coldef import ColDef
+from utils.crud.parser import get_python_type_parse
 from core import i18n as _
+from utils.crud import BaseApiOut
+from utils.crud.utils import parser_item_id
 from utils.log import log as log
 
 class ColAdmin(admin.ModelAdmin):
@@ -30,6 +47,51 @@ class ColAdmin(admin.ModelAdmin):
 
     def __init__(self, app: "AdminApp"):
         # 初始化
-        log.debug('app = %s' % app)
+        #log.debug('app = %s' % app)
         # app: apps.admin.admin.HostAdminApp
         super().__init__(app)
+
+    @property
+    def route_create(self) -> Callable:
+        async def route(
+            request: Request,
+            data: Union[self.schema_create, List[self.schema_create]] = Body(...),  # type: ignore
+        ) -> BaseApiOut[Union[int, self.schema_model]]:  # type: ignore
+            if not await self.has_create_permission(request, data):
+                return self.error_no_router_permission(request)
+            if not isinstance(data, list):
+                data = [data]
+            items = [await self.on_create_pre(request, obj) for obj in data]
+            for item in items:
+                if item.__contains__('createdate'):
+                    item['createdate'] = datetime_util.now()
+                if item.__contains__('modifiedate'):
+                    item['modified'] = datetime_util.now()
+            if not items:
+                return self.error_data_handle(request)
+            try:
+                result = await self.db.async_run_sync(self._create_items, items=items)
+            except Exception as error:
+                return self.error_execute_sql(request=request, error=error)
+            return BaseApiOut(data=result)
+        return route
+
+    @property
+    def route_update(self) -> Callable:
+        async def route(
+                request: Request,
+                item_id: List[str] = Depends(parser_item_id),
+                data: self.schema_update = Body(...),  # type: ignore
+        ):
+            if not await self.has_update_permission(request, item_id, data):
+                return self.error_no_router_permission(request)
+            item_id = list(map(get_python_type_parse(self.pk), item_id))
+            values = await self.on_update_pre(request, data, item_id=item_id)
+            if values.__contains__('modifiedate'):
+                values['modifiedate'] = datetime_util.now()
+            if not values:
+                return self.error_data_handle(request)
+            result = await self.db.async_run_sync(self._update_items, item_id, values)
+            return BaseApiOut(data=result)
+
+        return route
--- models/db_models/coldef.py
+++ models/db_models/coldef.py
@@ -10,8 +10,11 @@
 #  @Software: Practitioner
 
 from sqlmodel import Column, JSON
+
+from utils import datetime_util
 from utils.amis_admin import models, amis
-from typing import Optional
+from typing import TYPE_CHECKING,Optional, List
+import simplejson as json
 import sqlmodel
 from core import i18n as _
 
@@ -28,17 +31,32 @@ class ColDef(BaseSQLModel, table=True):
     coldef_name: str = models.Field(title=_('coldef_name'), nullable=False,
                                        amis_form_item=amis.InputText(name='coldef_name', label=_('coldef_name'), unique='true', disabled=False),
                                        amis_table_column='')
+    coldef: Optional[List[dict]] = models.Field(default=None, index=False, title=_('coldef'), nullable=False,
+                                                sa_column=Column(JSON),
+                                                amis_form_item=amis.Combo(type='combo', label=_('coldef'), id='input_combo_coldef',
+                                                                          items=[
+                                                    amis.InputText(name='name', label=_('Name'), unique='true'),
+                                                    amis.InputText(name='type', label=_('Type'), options=[{"label":_("bool"),"value":"bool"},{"label":_("number"),"value":"number"},{"label":_("string"),"value":"string"}], creatable=False, clearable=False, resetValue='string'),
+                                                    amis.Switch(name='required', label=_('Required'), onText=_('Yes'), offText=_('No'), trueValue=True, falseValue=False),
+                                                    amis.Switch(name='allow_none', label=_('Allow_None'), onText=_('Yes'), offText=_('No'), trueValue=True, falseValue=False)
+                                                                          ],
+                                                                          canAccessSuperData=True,
+                                                                          tabsMode=False,
+                                                                          addable=True,
+                                                                          removable=True,
+                                                                          tabsStyle='line',
+                                                                          multiple=True,
+                                                                          draggable=True,
+                                                                          deleteConfirmText=_('Confirm to delete?'),
+                                                                          tabsLabelTpl='${coldef[${index}].name}'),
+                                                amis_table_column=amis.TableColumn(type='json', levelExpand=0))
     keyfieldname: str = models.Field(title=_('keyfieldname'), nullable=False,
-                                    amis_form_item=amis.InputText(name='keyfieldname', label=_('keyfieldname'),
-                                                                  unique='true', disabled=False),
+                                    amis_form_item=amis.InputText(name='keyfieldname', label=_('keyfieldname'), source='${coldef|pick:name}', creatable=False, clearable=False, disabled=False),
                                     amis_table_column='')
-    coldef: Optional[dict] = models.Field(default=None, title=_('coldef'), nullable=True, sa_column=Column(JSON),
-                                              amis_form_item=amis.Textarea(name='coldef', label=_('coldef')),
-                                              amis_table_column='')
-    createdate: Optional[str] = models.Field(default=None, title=_('createdate'), nullable=True,
-                                          amis_form_item=amis.InputDatetime(name='createdate', label=_('createdate'), inputFormat='YYYY-MM-DD HH:mm:ss'),
+    createdate: Optional[str] = models.Field(default=datetime_util.now(), title=_('createdate'), nullable=False,
+                                          amis_form_item=amis.InputDatetime(name='createdate', label=_('createdate'), inputFormat='YYYY-MM-DD HH:mm:ss', format='YYYY-MM-DD HH:mm:ss', value='now', disabled=True),
                                           amis_table_column=amis.TableColumn(type='date'))
-    modifiedate: Optional[str] = models.Field(default=None, title=_('modifiedate'), nullable=True,
-                                           amis_form_item=amis.InputDatetime(name='modifiedate', label=_('modifiedate'), inputFormat='YYYY-MM-DD HH:mm:ss'),
+    modifiedate: Optional[str] = models.Field(default=datetime_util.now(), title=_('modifiedate'), nullable=False,
+                                           amis_form_item=amis.InputDatetime(name='modifiedate', label=_('modifiedate'), inputFormat='YYYY-MM-DD HH:mm:ss', format='YYYY-MM-DD HH:mm:ss', value='now', disabled=True),
                                            amis_table_column=amis.TableColumn(type='date'))
 
